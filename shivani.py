cb7656a3c4b06a40872755397ab5250bd3e0f32b
shivani <shivani95larokar@gmail.com>
2025-09-24 03:42:12 +0530

    shivani

---
+++ shivani.py
@@ -0,0 +1,8 @@
+# function to find the largest of three numbers
+def largest_of_three(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c    
