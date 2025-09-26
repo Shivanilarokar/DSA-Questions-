97c1feffddf854993aaf8e8375241c613332b132
shivani <shivani95larokar@gmail.com>
2025-09-26 20:32:00 +0530

    nnnndx

--- shivani.py
+++ shivani.py
@@ -16,22 +16,3 @@ class User:
         self.name = name
         self.borrowed_books = []
 
-    def borrow_book(self, book):
-        if book.copies > 0:
-            book.copies -= 1
-            self.borrowed_books.append((book, datetime.date.today()))
-            print(f"📚 {self.name} borrowed '{book.title}'")
-        else:
-            print(f"❌ Sorry, '{book.title}' is not available.")
-
-    def return_book(self, book):
-        for borrowed, date in self.borrowed_books:
-            if borrowed == book:
-                book.copies += 1
-                self.borrowed_books.remove((borrowed, date))
-                print(f"✅ {self.name} returned '{book.title}'")
-                return
-        print(f"⚠️ {self.name} has not borrowed '{book.title}'.")
-
-    def __str__(self):
-        return f"User: {self.na
